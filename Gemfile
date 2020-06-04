280f48091028d8ef0b483046ef9d75fca93a6ec8
stigmat <sheludko.vo@gmail.com>
2020-06-04 16:31:45 +0300

    create views

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.1'
 
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
+gem 'twitter-bootstrap-rails'
 gem 'uglifier'
 gem 'jquery-rails'
 gem 'webpacker', '~> 4.0'
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ApplicationRecord
 
-	validates :title, presence: true, length: {maximum: 255}
+	belongs_to :user
+
+	validates :user, presence: true
+
+	validates :title, presence: true,
+	          length: {maximum: 255}
 	validates :address, presence: true
 	validates :datetime, presence: true
 end
