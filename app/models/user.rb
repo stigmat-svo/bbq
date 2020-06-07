9642114cbf288235a0c05ee1c2a1ffd917150653
stigmat <sheludko.vo@gmail.com>
2020-06-07 16:09:54 +0300

    bootstrap for devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+	devise :database_authenticatable, :registerable,
+	       :recoverable, :rememberable, :validatable
 
 	has_many :events, dependent: :destroy
 
 	validates :name, presence: true, length: {maximum: 40}
 
-  before_validation :set_name, on: :create
+	before_validation :set_name, on: :create
 
-  private
+	private
 
-  def set_name
-	  self.name = "Ваше имя" if self.name.blank?
-  end
+	def set_name
+		self.name = "Ваше имя" if name.blank?
+	end
 end
