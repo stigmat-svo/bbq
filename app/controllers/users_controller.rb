318a15da6de404612fd5f4e21877c191d8166ba6
stigmat <sheludko.vo@gmail.com>
2020-06-07 16:30:06 +0300

    bootstrap for devise forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, except: [:show]
-	before_action :set_current_user, except: [:show]
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_current_user, except: [:show]
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @user.update(user_params)
-			redirect_to @user, notice: 'Данные пользователя успешно обновлены!'
-		else
-			render :edit
-		end
-	end
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Данные пользователя успешно обновлены!'
+    else
+      render :edit
+    end
+  end
 
-	private
+  private
 
-	def set_current_user
-		@user = current_user
-	end
+  def set_current_user
+    @user = current_user
+  end
 
-	def user_params
-		params.require(:user).permit(:name, :email)
-	end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
-	belongs_to :user, optional: true
+  belongs_to :user, optional: true
 
-	validates :user, presence: true
+  validates :user, presence: true
 
-	validates :title, presence: true,
-	          length: {maximum: 255}
-	validates :address, presence: true
-	validates :datetime, presence: true
+  validates :title, presence: true,
+            length: {maximum: 255}
+  validates :address, presence: true
+  validates :datetime, presence: true
 end
