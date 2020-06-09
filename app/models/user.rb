ca8dad450c67c19e64344b22c66259d2dd5cd4fc
stigmat <sheludko.vo@gmail.com>
2020-06-09 21:46:18 +0300

    add comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
+  has_many :comments
 
   validates :name, presence: true, length: {maximum: 40}
 
@@ -11,6 +12,6 @@ class User < ApplicationRecord
   private
 
   def set_name
-    self.name = "Ваше имя" if name.blank?
+    self.name = "Посетитель" if name.blank?
   end
 end
