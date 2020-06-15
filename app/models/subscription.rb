abfcf3fecbdb87b81f32c98363ac25ceae473d8c
stigmat <sheludko.vo@gmail.com>
2020-06-15 17:52:52 +0300

    ban on self-subscription and organizer email

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -13,6 +13,10 @@ class Subscription < ApplicationRecord
 
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
 
+  validate :user_organizer, if: -> { user.present? }
+
+  validate :email_already_exists, unless: -> { user.present? }
+
   def user_name
     if user.present?
       user.name
@@ -26,6 +30,20 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def user_organizer
+    if user.id == event.user_id
+      errors.add(user_name, I18n.t('form.errors.user_organizer'))
+    end
+  end
+
+  def email_already_exists
+    if User.find_by(email: user_email)
+      errors.add('Выбранный e-mail', I18n.t('form.errors.email_already_exists'))
     end
   end
 end
