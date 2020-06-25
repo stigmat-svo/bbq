086b24b02c0b45d76180d7d43bdfe3f9f7304878
stigmat <sheludko.vo@gmail.com>
2020-06-25 23:32:44 +0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,14 +104,13 @@ Rails.application.configure do
 
   # Настройки для Sendgrid
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           =>  587,
-    :domain         => 'gmail.com',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address:         'smtp.sendgrid.net',
+    port:            '587',
+    authentication:  :plain,
+    user_name:        ENV['SENDGRID_USERNAME'],
+    password:         ENV['SENDGRID_PASSWORD'],
+    domain:          'heroku.com',
+    enable_starttls_auto:  true
   }
 
   # Inserts middleware to perform automatic connection switching.
