eceeef374c40b6afbb95933c987d19da0060ac10
stigmat <sheludko.vo@gmail.com>
2020-06-25 19:34:00 +0300

    eventmailer, sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,8 +107,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app174227565@heroku.com'],
-    :password       => ENV['rznmqhrx5303'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
