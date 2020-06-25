511bc6cce31057df2cb4c49fd92c51ccd3dabc5b
stigmat <sheludko.vo@gmail.com>
2020-06-25 23:19:43 +0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,9 +104,9 @@ Rails.application.configure do
 
   # Настройки для Sendgrid
   ActionMailer::Base.smtp_settings = {
-    :from           => 'pupa.programmer@gmail.com',
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
+    :domain         => 'party-point.herokuapp.com',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
