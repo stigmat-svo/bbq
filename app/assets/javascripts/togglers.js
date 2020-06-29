25ce0b543e57d610b5dbc129048ae7ce78591f14
stigmat <sheludko.vo@gmail.com>
2020-06-30 00:45:02 +0300

    add yandex map

--- app/assets/javascripts/togglers.js
+++ app/assets/javascripts/togglers.js
@@ -1,27 +1,25 @@
 $(".event-tools").ready(function(){
-  $('#subscribe').hide()
-  $('#subscribe-on').click(function(){
-    $('#subscribe').slideToggle(500);
-    $('#comments').hide()
-    $('#gallery').hide()
-    $('#map').hide()
-    return false;
-  });
-
-  $('#comments').hide()
-  $('#comment-on').click(function(){
-    $('#comments').slideToggle(500);
-    $('#subscribe').hide()
-    $('#gallery').hide()
-    $('#map').hide()
-    return false;
-  });
+  // $('#subscribe').hide()
+  // $('#subscribe-on').click(function(){
+  //   $('#subscribe').slideToggle(500);
+  //   $('#comments').hide()
+  //   $('#gallery').hide()
+  //   $('#map').hide()
+  //   return false;
+  // });
+  //
+  // $('#comments').hide()
+  // $('#comment-on').click(function(){
+  //   $('#comments').slideToggle(500);
+  //   $('#subscribe').hide()
+  //   $('#gallery').hide()
+  //   $('#map').hide()
+  //   return false;
+  // });
 
   $('#gallery').hide()
   $('#gallery-on').click(function(){
     $('#gallery').slideToggle(500);
-    $('#subscribe').hide()
-    $('#comments').hide()
     $('#map').hide()
     return false;
   });
@@ -29,8 +27,6 @@ $(".event-tools").ready(function(){
   $('#map').show()
   $('#map-on').click(function(){
     $('#map').slideToggle(500);
-    $('#subscribe').hide()
-    $('#comments').hide()
     $('#gallery').hide()
     return false;
   });
