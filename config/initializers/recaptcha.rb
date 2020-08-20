276390d4a9f65ed9fbe6114513a62398d84f92ee
stigmat <sheludko.vo@gmail.com>
2020-08-20 18:10:29 +0300

    deploy

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_PARTY-POINT_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_PARTY-POINT_PRIVATE_KEY']
+  config.site_key  = ENV['RECAPTCHA_BBQ_PUBLIC_KEY']
+  config.secret_key = ENV['RECAPTCHA_BBQ_PRIVATE_KEY']
 end
